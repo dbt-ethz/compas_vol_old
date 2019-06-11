3e3846074fe8079ed6cdcb36c7c80fe930f68fa2
worbit <bernhard@arch.ethz.ch>
2019-06-11 07:45:32 +0200

    implement octree (SVO)

--- src/compas_vol/modifications/transform.py
+++ src/compas_vol/modifications/transform.py
@@ -20,9 +20,11 @@ if __name__ == "__main__":
     from compas_vol.primitives import Box
     from compas.geometry import Frame
 
-    b = Box(25,20,15,7)
-    f1 = Frame([1, 1, 1], [0.68, 0.68, 0.27], [-0.67, 0.73, -0.15])
+    b = Box(25,20,15,5)
+    #f1 = Frame([1, 1, 1], [0.68, 0.68, 0.27], [-0.67, 0.73, -0.15])
+    f1 = Frame([0,0,0],[1,0.2,0],[-0.2,1,0])
     T = Transformation.from_frame(f1)
+    # alternatively: from_matrix, from_list
     t = Transform(b,T)
     for y in range(-15,15):
         s = ''
