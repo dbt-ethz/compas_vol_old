80edba407a77e5c898cc14e8ff7b1a87f5da3b78
worbit <bernhard@arch.ethz.ch>
2019-06-27 14:42:23 +0200

    formatting whitespaces

--- src/compas_vol/combinations/intersection.py
+++ src/compas_vol/combinations/intersection.py
@@ -1,28 +1,29 @@
 class Intersection(object):
     def __init__(self, a=None, b=None):
-        if type(a)==list:
+        if type(a) == list:
             self.objs = a
         else:
             self.objs = [a, b]
-    
-    def get_distance(self,x,y,z):
-        ds = [o.get_distance(x,y,z) for o in self.objs]
+
+    def get_distance(self, x, y, z):
+        ds = [o.get_distance(x, y, z) for o in self.objs]
         return max(ds)
 
 # ==============================================================================
 # Main
 # ==============================================================================
 
+
 if __name__ == "__main__":
     from compas_vol.primitives import Sphere, Box
     s = Sphere(9)
-    b = Box(25,10,10)
-    u = Intersection(s,b)
-    for y in range(-15,15):
+    b = Box(25, 10, 10)
+    u = Intersection(s, b)
+    for y in range(-15, 15):
         s = ''
-        for x in range(-30,30):
-            d = u.get_distance(x*0.5,y,0)
-            if d<0:
+        for x in range(-30, 30):
+            d = u.get_distance(x * 0.5, y, 0)
+            if d < 0:
                 s += 'x'
             else:
                 s += '·'
--- src/compas_vol/combinations/smoothunion.py
+++ src/compas_vol/combinations/smoothunion.py
@@ -3,19 +3,19 @@ class SmoothUnion(object):
         self.a = a
         self.b = b
         self.r = r
-    
-    def get_distance_(self,x,y,z):
-        da = self.a.get_distance(x,y,z)
-        db = self.b.get_distance(x,y,z)
-        e = max(self.r - abs(da-db), 0)
-        return min(da,db) - e*e*0.25/self.r
-    
-    def get_distance(self,x,y,z):
-        da = self.a.get_distance(x,y,z)
-        db = self.b.get_distance(x,y,z)
+
+    def get_distance_(self, x, y, z):
+        da = self.a.get_distance(x, y, z)
+        db = self.b.get_distance(x, y, z)
+        e = max(self.r - abs(da - db), 0)
+        return min(da, db) - e**2 * 0.25 / self.r
+
+    def get_distance(self, x, y, z):
+        da = self.a.get_distance(x, y, z)
+        db = self.b.get_distance(x, y, z)
         k = self.r
-        h = min(max( 0.5+0.5*(db-da)/k ,0),1)
-        return (db*(1-h) + h*da) - k*h*(1-h)
+        h = min(max(0.5 + 0.5 * (db - da) / k, 0), 1)
+        return (db * (1 - h) + h * da) - k * h * (1 - h)
 
 
 # ==============================================================================
@@ -25,13 +25,13 @@ class SmoothUnion(object):
 if __name__ == "__main__":
     from compas_vol.primitives import Sphere, Box
     s = Sphere(9)
-    b = Box(25,10,10)
-    u = SmoothUnion(s,b,1.5)
-    for y in range(-15,15):
+    b = Box(25, 10, 10)
+    u = SmoothUnion(s, b, 1.5)
+    for y in range(-15, 15):
         s = ''
-        for x in range(-30,30):
-            d = u.get_distance(x*0.5,y,0)
-            if d<0:
+        for x in range(-30, 30):
+            d = u.get_distance(x*0.5, y, 0)
+            if d < 0:
                 s += 'x'
             else:
                 s += '·'
