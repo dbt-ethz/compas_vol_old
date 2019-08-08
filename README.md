14da73ab8af2bbf03fcc174a16c17d480171b5e3
worbit <bernhard@arch.ethz.ch>
2019-08-08 14:06:11 +0200

    update readme

--- README.md
+++ README.md
@@ -1,4 +1,32 @@
 # compas_vol
+## Volumetric Modelling
 
 volumetric modelling with function representations
 
+## Installation Instructions
+
+To get started with compas development, I recommend you create a separate environment in an anaconda python 3.7 installation. The details are described here: https://compas-dev.github.io/main/gettingstarted/installation.html
+
+Long story short, the basic steps you need to take are the following:
+- preparation
+ - `$ conda config --add channels conda-forge`
+- ~~create environment for dev and install compas~~
+ - ~~`$ conda create -n compas-dev python=3.7 COMPAS`~~
+- create a new environment for compas development
+ - `$ conda create -n compas-dev python=3.7`
+- activate the new environment
+ - `$ conda activate compas-dev`
+- clone compas core and install it from the local copy
+ - `$ git clone git@github.com:compas-dev/compas.git`
+ - `$ cd compas`
+ - `$ pip install e .`
+- navigate to a folder where you want the local copy of `compas_vol`
+- clone the repository (you should have gotten an invitation)
+ - `$ git clone git@github.com:dbt-ethz/compas_vol.git`
+- install the package for the current environment
+ - `$ pip install -r requirements-dev.txt`
+- furthermore, you will need pyside, opengl, scikit-image and ev. ipyvolume
+ - `$ conda install pyopengl`
+ - `$ conda install -c conda-forge pyside2`
+ - `$ conda install scikit-image`
+ - `$ conda install -c conda-forge ipyvolume`
--- src/compas_vol/analysis/gradient.py
+++ src/compas_vol/analysis/gradient.py
@@ -3,6 +3,18 @@ from compas.geometry import Vector
 
 
 class Gradient(object):
+    """
+    A Gradient object calculates the ∇ (Nabla), or the direction vector of
+    the gradient of a distance function. It is perpendicular to the 0-level
+    iso-surface for points on the boundary.
+
+    Parameters
+    ----------
+    o : :class:`compas_vol` distance object.
+        The object with the signed distance function to calculate the gradient from.
+    e : float
+        Epsilon, the offset (+/-) for the central distance calculation.
+    """
     def __init__(self, o, e=0.01):
         self.o = o
         self.e = e
@@ -18,10 +30,20 @@ class Gradient(object):
         v.unitize()
         return v
 
+    def get_gradient_numpy(self, x, y, z):
+        import numpy as np
+
+        dx = self.o.get_distance_numpy(x + self.e, y, z) - self.o.get_distance_numpy(x - self.e, y, z)
+        dy = self.o.get_distance_numpy(x, y + self.e, z) - self.o.get_distance_numpy(x, y - self.e, z)
+        dz = self.o.get_distance_numpy(x, y, z + self.e) - self.o.get_distance_numpy(x, y, z - self.e)
+        return np.array([dx, dy, dz]).T
+
 
 if __name__ == "__main__":
     from compas_vol.primitives import VolSphere
     from compas.geometry import Sphere
+    import numpy as np
+    import ipyvolume as ipv
 
     s = Sphere(Point(1, 2, 3), 4)
     vs = VolSphere(s)
@@ -29,3 +51,11 @@ if __name__ == "__main__":
     g = Gradient(vs)
     print(vs.get_distance(Point(4, 5, 6)))
     print(g.get_gradient(Point(5, 2, 3)))
+
+    x, y, z = np.ogrid[-10:10:20j, -10:10:20j, -10:10:20j]
+    d = g.get_gradient_numpy(x, y, z)
+    print(type(d), d.shape)
+
+    ipv.figure()
+    ipv.quiver(x, y, z, d[:,:,:,0], d[:,:,:,1], d[:,:,:,2])
+    ipv.show()
