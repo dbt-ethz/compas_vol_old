79c10aa20c259cabeebb119e821ac5a24823ef2d
worbit <bernhard@arch.ethz.ch>
2020-06-17 11:58:35 +0200

    first version of lattice polar (no numpy)

--- src/compas_vol/microstructures/lattice_polar.py
+++ src/compas_vol/microstructures/lattice_polar.py
@@ -6,7 +6,7 @@ from compas.geometry import matrix_inverse
 from compas import PRECISION
 
 
-class Lattice(object):
+class LatticePolar(object):
     """A lattice is defined by it's type, size of a unit cell and its strut diameter.
     Optionally, a frame can be specified to modify orientation and origin.
 
@@ -27,13 +27,14 @@ class Lattice(object):
     >>> lat.frame = Frame((1, 0, 0), (1, 0.2, 0.1), (-0.3, 1, 0.2))
 
     """
-    def __init__(self, ltype=0, unitcell=1.0, thickness=0.1, frame=Frame.worldXY()):
+    def __init__(self, ltype=0, unitcell=1.0, thickness=0.1, polarnumber=6, frame=Frame.worldXY()):
         self.pointlist = self.create_points()
         self.ltypes = self.create_types()
         self._ltype = None
         self.ltype = ltype
         self.unitcell = unitcell
         self.thickness = thickness
+        self.polarnumber = polarnumber
         self._frame = None
         self.frame = frame
         transform = matrix_from_frame(self.frame)
@@ -147,9 +148,27 @@ class Lattice(object):
         else:
             pt = point
 
+        '''
+        double radius = Math.Sqrt(x * x + y * y);
+        double pX = Math.Abs(ModuloPositive(radius, cellsize) - halfCellsize);
+        double angle = (Math.Atan2(y, x) + Math.PI) / (2 * Math.PI); // normalized 0 : 1
+        double pY = (angle * this.polar) % 1;
+        pY = Math.Abs((pY - 0.5) * this.cellsize);
+        double pZ = Math.Abs(ModuloPositive(z, cellsize) - halfCellsize);
+        '''
+
         pt.transform(self.inversetransform)
 
-        up = [abs((p % self.unitcell) - self.unitcell/2) for p in pt]
+        radius = math.sqrt(pt.x * pt.x + pt.y * pt.y)
+        polx = abs(radius % self.unitcell) - self.unitcell/2
+        angle = (math.atan2(pt.y, pt.x) + math.pi) / (2 * math.pi)
+        poly = (angle * self.polarnumber) % 1
+        poly = abs((poly - 0.5) * self.unitcell)
+        polz = abs(pt.z % self.unitcell) - self.unitcell/2
+
+        up = [polx, poly, polz]
+
+        #up = [abs((p % self.unitcell) - self.unitcell/2) for p in pt]
         dmin = 9999999.
         for l in self.ltypes[self.ltype]:
             sp = [self.pointlist[l[0]][i] * self.unitcell for i in range(3)]
@@ -173,8 +192,15 @@ class Lattice(object):
 
         p = np.array([x, y, z, 1])
         md = np.dot(self.inversetransform, p)
-        mg = np.stack(([md[0], md[1], md[2]]), axis=-1)
-        mg = abs((mg % self.unitcell) - self.unitcell/2)
+
+        radius = np.sqrt(md[0]**2 + md[1]**2)
+        polx = abs((radius % self.unitcell) - self.unitcell/2)
+        angle = np.arctan2(md[1], md[2])
+        poly = (angle * self.polarnumber) % 1
+        poly = abs((poly - 0.5) * self.unitcell)
+        polz = abs((md[2] % self.unitcell) - self.unitcell/2)
+        mg = np.stack(([polx, poly, polz]), axis=-1)
+        #mg = abs((mg % self.unitcell) - self.unitcell/2)
 
         distances = []
         for l in self.ltypes[self.ltype]:
@@ -186,28 +212,20 @@ class Lattice(object):
 
 
 if __name__ == "__main__":
-    import numpy as np
-    import matplotlib.pyplot as plt
-    from compas.geometry import Vector, Frame
+    # import numpy as np
+    # import matplotlib.pyplot as plt
+    # from compas.geometry import Vector, Frame
 
-    lat = Lattice(7, 5.0, 0.5)
-    lat.frame = Frame((1, 0, 0), (1, 0.2, 0.1), (-0.3, 1, 0.2))
-
-    print(lat.typenames, lat.lattice_type)
-    print(lat)
-    lat2 = eval(str(lat))
-
-    x, y, z = np.ogrid[-14:14:112j, -12:12:96j, -10:10:80j]
-
-    m = lat2.get_distance_numpy(x, y, z)
-
-    # num = 200
-    # m = np.empty((num, num))
-    # for r in range(num):
-    #     for c in range(num):
-    #         m[r, c] = lat.get_distance((c, r, 10))
-
-    plt.imshow(m[:, :, 25].T, cmap='RdBu')  # transpose because numpy indexing is 1)row 2) column instead of x y
-    plt.colorbar()
-    plt.axis('equal')
-    plt.show()
+    lat = LatticePolar(5, 7.0, 2.5, 6)
+    print(lat.ltype)
+    #lat.frame = Frame((1, 0, 0), (1, 0.2, 0.1), (-0.3, 1, 0.2))
+    for y in range(-15, 15):
+        s = ''
+        for x in range(-30, 30):
+            d = lat.get_distance(Point(x*0.5, y, 2))
+            #print(d)
+            if d < 0:
+                s += 'x'
+            else:
+                s += '.'
+        print(s)
