01fa3e89cfd8c27f4aa2dc29ed67ee36346a1d44
Remy Clemente <clementeremy@gmail.com>
2020-06-19 11:31:22 +0200

    Update tpms_polar.py

--- src/compas_vol/microstructures/tpms_polar.py
+++ src/compas_vol/microstructures/tpms_polar.py
@@ -149,11 +149,11 @@ if __name__ == "__main__":
     import numpy as np
     import matplotlib.pyplot as plt
 
-    tpmsPol = TPMSPolar(0, 4.0,3.0, 10.0)
+    tpmsPol = TPMSPolar(1, 4.0,3.0, 10.0)
 
     x, y, z = np.ogrid[-14:14:112j, -12:12:96j, -10:10:80j]
     m = tpmsPol.get_distance_numpy(x, y, z)
-    print(m)
+    # print(m)
     plt.imshow(m[:, :, 25].T, cmap='RdBu')  # transpose because numpy indexing is 1)row 2) column instead of x y
     plt.colorbar()
     plt.axis('equal')
