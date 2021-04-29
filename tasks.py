bd90e767847f1a8b7536394d85b68c10f754ae27
Tom Van Mele <brg@arch.ethz.ch>
2021-04-29 19:05:49 +0200

    Update tasks.py
    Co-authored-by: Gonzalo Casas <gonzalocasas@gmail.com>

--- tasks.py
+++ tasks.py
@@ -90,7 +90,7 @@ def clean(ctx, docs=True, bytecode=True, builds=True):
 
         if builds:
             folders.append('build/')
-            folders.append('src/compas.egg-info/')
+            folders.append('src/compas_vol.egg-info/')
 
         for folder in folders:
             shutil.rmtree(os.path.join(BASE_FOLDER, folder), ignore_errors=True)
