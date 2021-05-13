b776544dfc549062860479eee92900c5c765280c
worbit <bernhard@arch.ethz.ch>
2021-05-13 09:50:17 -0400

    add quadratic blend

--- src/compas_vol/combinations/blend.py
+++ src/compas_vol/combinations/blend.py
@@ -2,11 +2,12 @@ from compas import PRECISION
 
 
 class Blend(object):
-    def __init__(self, a=None, b=None, c=None, r=1.0):
+    def __init__(self, a=None, b=None, c=None, r=1.0, t=0):
         self.a = a
         self.b = b
         self.c = c
         self.r = r
+        self.t = t
 
     def __repr__(self):
         return "Blend({0},{1},{2},{3:.{3}f})".format(str(self.a), str(self.b), str(self.c), self.r, PRECISION[:1])
@@ -24,7 +25,11 @@ class Blend(object):
             return db
         else:
             f = dc / self.r + 0.5
-            return (1 - f) * da + f * db
+            if self.t==0:
+                return (1 - f) * da + f * db
+            elif self.t==1:
+                qf = 2 * f**2 if f < 0.5 else 1 - pow(-2 * f + 2, 2) / 2
+                return (1 - qf) * da + qf * db
     
     def get_distance_numpy(self, x, y, z):
         """
