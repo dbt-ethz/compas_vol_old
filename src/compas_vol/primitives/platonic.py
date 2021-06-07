60ab72a2ba6b1f7e195656e26f77aa6d926322d9
worbit <bernhard@arch.ethz.ch>
2021-06-07 09:11:41 -0400

    platonics distance functions

--- src/compas_vol/primitives/platonic.py
+++ src/compas_vol/primitives/platonic.py
@@ -1,4 +1,5 @@
 from compas.geometry import Point
+from math import sqrt, tan, pi
 
 class PlatonicSolid(object):
     """A platonic solid, defined by radius and type.
@@ -18,10 +19,22 @@ class PlatonicSolid(object):
     def __init__(self, radius, type=0):
         self.radius = radius;
         self.type = type;
+        self.sqrt3 = sqrt(3)
+        self.tan30 = tan(pi/6)
     
     def get_distance(self, point):
-        return 0
-    
+        x, y, z = point
+        if self.type == 0:
+            return (max(abs(x + y) - z, abs(x - y) + z) - self.radius) / self.sqrt3
+        elif self.type == 1:
+            s = abs(x) + abs(y) + abs(z)
+            # s = sum([abs(v) for v in point])
+            return (s - self.radius) * self.tan30
+        elif self.type == 2:
+            return 0
+        else:
+            return 0
+        
     def get_distance_numpy(self, x, y, z):
         import numpy as np
         return 0
