4f1e17141fb3976951c9a60f31991339aa7d9f4b
worbit <bernhard@arch.ethz.ch>
2021-06-10 07:23:05 -0400

    dodecahedron and test

--- src/compas_vol/primitives/platonic.py
+++ src/compas_vol/primitives/platonic.py
@@ -1,4 +1,4 @@
-from compas.geometry import Point
+from compas.geometry import Point, Vector
 from math import sqrt, tan, pi
 
 class PlatonicSolid(object):
@@ -14,6 +14,12 @@ class PlatonicSolid(object):
     Examples
     --------
     >>> ...
+
+    References
+    ----------
+    adapted from SDF library by Michael Fogleman [1]_
+
+    ..[1] https://github.com/fogleman/sdf/blob/main/sdf/d3.py#L283
     """
 
     def __init__(self, radius, type=0):
@@ -24,17 +30,50 @@ class PlatonicSolid(object):
     
     def get_distance(self, point):
         x, y, z = point
+        
+        # Tetrahedron
         if self.type == 0:
             return (max(abs(x + y) - z, abs(x - y) + z) - self.radius) / self.sqrt3
+        
+        # Octahedron
         elif self.type == 1:
             s = abs(x) + abs(y) + abs(z)
             # s = sum([abs(v) for v in point])
             return (s - self.radius) * self.tan30
+        
+        # Dodecahedron
         elif self.type == 2:
+            v = Vector((1+sqrt(5))/2, 1, 0)
+            v.unitize()
+            px = abs(x / self.radius)
+            py = abs(y / self.radius)
+            pz = abs(z / self.radius)
+            p = Vector(px, py, pz)
+            a = p.dot(v)
+            b = p.dot(Vector(v.z, v.x, v.y))
+            c = p.dot(Vector(v.y, v.z, v.x))
+            q = (max(max(a, b), c) - v.x) * self.radius
+            return q
+        
+        # Icosahedron
+        elif self.type == 3:
             return 0
+        
         else:
             return 0
         
     def get_distance_numpy(self, x, y, z):
         import numpy as np
         return 0
+
+if __name__=="__main__":
+    p = PlatonicSolid(10.0, 0)
+    for y in range(-15, 15):
+        s = ''
+        for x in range(-30, 30):
+            d = p.get_distance((x * 0.5, -y, 0))
+            if d < 0:
+                s += 'x'
+            else:
+                s += '.'
+        print(s)
