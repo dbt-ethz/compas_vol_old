43b0c656dc331356d456abacb14336904f509a96
Remy Clemente <clementeremy@gmail.com>
2021-07-31 20:14:16 +0200

    Update example_tpms.py
    Test with meshplot display

--- examples/example_tpms.py
+++ examples/example_tpms.py
@@ -1,19 +1,24 @@
-# from compas.geometry import Point
 import numpy as np
+import meshplot as mp
+import ipyvolume as ipv
 import matplotlib.pyplot as plt
 from compas_vol.microstructures import TPMS
 
-b = TPMS(tpmstype='schwartzP', wavelength=5)
-print(b)
-
 x, y, z = np.ogrid[-14:14:112j, -12:12:96j, -10:10:80j]
-
+b = TPMS(tpmstype='schwartzP', wavelength=5)
 m = b.get_distance_numpy(x, y, z)
 
-plt.imshow(m[:, :, 25].T, cmap='RdBu')  # transpose because numpy indexing is 1)row 2) column instead of x y
-plt.colorbar()
-plt.axis('equal')
-plt.show()
+ipv_mesh = ipv.plot_isosurface(m, 0.0)
+print(ipv_mesh.keys)
+# v = np.empty((ipv_mesh.x.size,3))
+# v[:,0], v[:,1], v[:,2] = ipv_mesh.x, ipv_mesh.y, ipv_mesh.z
+# f = ipv_mesh.triangles
+# mp.plot(v, f)
+
+# plt.imshow(m[:, :, 25].T, cmap='RdBu')  # transpose because numpy indexing is 1)row 2) column instead of x y
+# plt.colorbar()
+# plt.axis('equal')
+# plt.show()
 
 # for y in range(-15, 15):
 #     s = ''
