d9e11fff8d8e5bbd5c7f753531bd836ab2859ef5
Remy Clemente <clementeremy@gmail.com>
2021-11-23 13:10:50 +0100

    Update smoothunionlist.py

--- src/compas_vol/combinations/smoothunionlist.py
+++ src/compas_vol/combinations/smoothunionlist.py
@@ -37,13 +37,22 @@ class SmoothUnionList(object):
         return -log(res, 2) / self.k
 
     def get_distance_numpy(self, x, y, z):
-        raise NotImplementedError
+        import numpy as np
+
+        distances = np.array([o.get_distance_numpy(x, y, z) for o in self.distance_objects]).T
+        res = np.sum(np.power(2, np.full((distances.shape), -self.k) * distances), axis=3)
+        return -np.log2(res)/self.k
+
+        # return np.minimum.reduce(distances)
+        # raise NotImplementedError
 
 
 if __name__ == "__main__":
     from compas.geometry import Point, Sphere
     from compas_vol.primitives import VolSphere
+    import matplotlib.pyplot as plt
     import random
+    import numpy as np
 
     spheres = []
     for i in range(10):
@@ -54,14 +63,21 @@ if __name__ == "__main__":
         vs = VolSphere(s)
         spheres.append(vs)
     
-    sul = SmoothUnionList(spheres, 0.8)
+    sul = SmoothUnionList(spheres, 2)
 
-    for y in range(-15, 15):
-        s = ''
-        for x in range(-30, 30):
-            d = sul.get_distance((x * 0.5, -y, 0))
-            if d < 0:
-                s += 'x'
-            else:
-                s += '.'
-        print(s)
+    x, y, z = np.ogrid[-15:15:61j, -15:15:61j, -15:15:61j]
+    d = sul.get_distance_numpy(x, y, z)
+    print (d)
+    plt.imshow(d[:, :, 0], cmap='RdBu')
+    plt.axis('equal')
+    plt.show()
+
+    # for y in range(-15, 15):
+    #     s = ''
+    #     for x in range(-30, 30):
+    #         d = sul.get_distance((x * 0.5, -y, 0))
+    #         if d < 0:
+    #             s += 'x'
+    #         else:
+    #             s += '.'
+    #     print(s)
